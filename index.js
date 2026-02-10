3d6c43c65284d2a6e85309b424be9ca362573e8f
patinhocodador99 <sthefanie.silvaifc00@gmail.com>
2026-02-10 03:23:33 +0000

    Adiciona rotas para obter personagens e suas informações por ID

--- index.js
+++ index.js
@@ -10,6 +10,20 @@ app.get('/oi', (req, res) => {
     res.send('Olá, mundo!')
 })
 
+const lista = ['Rick Sanchez', 'Morty Smith', 'Beth Smith']
+
+app.get('/personagens', (req, res) => {
+  res.send(lista)
+})
+
+app.get('/personagens/:id', (req, res) => {
+  const id = req.params.id
+    
+  const personagem = lista[id - 1]
+    
+  res.send(personagem)
+})
+
 app.listen(3000, () => { //iniciando o servidor
   console.log('Server is running on http://localhost:3000')
 })
